e2052df191e3060e4f87d0d0f1b512d20226dedf
Rafael Brandão <r.augusto.brandao@gmail.com>
2019-09-10 11:36:00 -0400

    updated mongo init script

--- init_user_and_data.sh
+++ init_user_and_data.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-ROOT_USER=admin
-ROOT_PASS=pass
-
 mongo -u $MONGO_INITDB_ROOT_USERNAME -p $MONGO_INITDB_ROOT_PASSWORD  --authenticationDatabase admin --eval "db.getSiblingDB('${MONGODB_DATABASE}').createUser({ user: '${MONGODB_USER}', pwd: '${MONGODB_PASS}', roles: [ { role: 'readWrite', db: '${MONGODB_DATABASE}' } ] });"
 
 mongoimport --db $MONGODB_DATABASE --collection chat -u $MONGODB_USER -p $MONGODB_PASS < data.json
